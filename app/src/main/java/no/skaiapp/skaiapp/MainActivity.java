dc4b67db506d3d87b5e897d60d31111df7570696
Aslak Frafjord Skailand <AslakFrafjord.Skailand@Altibox.no>
2021-11-15 10:04:18 +0100

    Added internet permisson to android manifest. Sending simple request to get joke of the day: https://api.jokes.one.

--- app/src/main/java/no/skaiapp/skaiapp/MainActivity.java
+++ app/src/main/java/no/skaiapp/skaiapp/MainActivity.java
@@ -7,6 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -21,8 +28,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         flippy = true;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         dateTimeDisplay = (TextView)findViewById(R.id.dateTimeDisplay);
         calendar = Calendar.getInstance();
         dateFormat = new SimpleDateFormat("MM/dd/yyyy - hh:mm");
@@ -31,8 +40,25 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.mainButton);
         textView = (TextView)findViewById(R.id.textView);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://api.jokes.one/jod";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+            @Override
+            public void onResponse(String response) {
+                System.out.println(response.substring(0, 400));
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("that did not work");
+            }
+        });
+
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                queue.add(stringRequest);
                 System.out.println("Hello, world!");
                 if (flippy) {
                     textView.setText("Aslak e best!");
